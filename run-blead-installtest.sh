0bf00a9ed26f2180b77d469d1f49993f528df935
Max Maischein <corion@corion.net>
2024-12-08 08:26:03 +0100

    Add the changes from the 5.39 run

--- run-blead-installtest.sh
+++ run-blead-installtest.sh
@@ -10,7 +10,6 @@ BASE=$(cd $(dirname $0); pwd)
 # We assume bash, so $PWD is set ...
 BLEADPERL=${1-$PWD}
 CONFIGURE=${CONFIGURE:-$BASE/cConfigure -- ./Configure}
-INSTALL_TARGET=$TEMP/perl-$VERSION
 
 if [ ! -f "$BLEADPERL/Configure" ]; then
     echo "'$BLEADPERL/Configure' not found - is this the right directory?"
@@ -20,12 +19,21 @@ fi
 # Find the current version of Perl we will build. This is maybe a bit
 # roundabout... Also, we require a Perl to be available already:
 cd "$BLEADPERL"
-# Hmm ... this reports 5.39.7 on a 5.39.6 bleadperl...
 DEFAULT_VERSION=$(perl -E 'require "./Porting/pod_lib.pl"; my $state=get_pod_metadata(); say sprintf "%d.%d.%d", @{$state->{delta_version}}')
 VERSION=${2-$DEFAULT_VERSION}
 
 if [[ -z "$VERSION" ]]; then
     echo "No version found?!"
+fi
+
+TEMP=${TMP-/tmp}
+INSTALL_TARGET=$TEMP/perl-$VERSION
+
+# Do a sanity check - if a tag for our version already exists, something is wrong:
+TAGGED=$(git tag -l "v$VERSION")
+if [[ ! -z $TAGGED ]]; then
+    echo "Version $VERSION is already tagged at $TAGGED"
+    exit 1
 fi
 
 # Update the terminal title with the config
@@ -34,7 +42,7 @@ echo -en "\e]2;Building $VERSION\a"
 
 # Should we clean and pull?!
 git clean -dfX
-git pull
+git pull github
 
 # clean out all the various local Perl modifications you might have.
 # We do this so the fresh Perl gets built clean and doesn't pollute your
@@ -51,6 +59,7 @@ export TEST_JOBS=8
 export MAKEFLAGS=-j12
 
 for conf in "" "-Dusethreads" "-Duserrelocatableinc" ; do
+    # Update our console title
     echo -en "\e]2;Building $VERSION $conf\a"
     rm config.sh Policy.sh
     if ! ( $CONFIGURE -Dusedevel -des $conf -Dprefix=$INSTALL_TARGET \
